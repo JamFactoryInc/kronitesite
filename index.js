40b9d13a2f5f1b74dc10d794da9a8ba6926f71fe
Kieran Smith <kieran.jas.smith@gmail.com>
2023-01-05 19:55:19 +1000

    implemented news and css CDN

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-const dce = Document.prototype.createElement;
 const links = { Steam: "a", Discord: "a", Reddit: "a", "Itch.io": "a", Gamejolt: "a" }
 
 window.addEventListener('load', () => {
     const n = document.getElementById("navbar");
     Object.entries(links).forEach((e) => {
-        const el = dce('span');
+        const el = document.createElement('span');
         el.innerHTML = `<a href="${e[1]}" target="_blank" rel="noopener noreferrer">${e[0]}</a>`;
         n.appendChild(el);
     })
     document.querySelectorAll(".pending.roadmapitem").forEach(e => {
-        const el = dce('span');
+        const el = document.createElement('span');
         el.className = "progress";
         el.style.width = e.getAttribute("pct");
         e.appendChild(el);
     })
 });
 
-function cdn(url, target, el) {
+function cdn(url, elType, fn) {
     fetch(url).then(res =>
         res.json().then(data =>
             data.forEach(e =>
-                fetch(e.download_url).then(t => {
-                    el.innerHTML = t;
-                    document.querySelector(target).appendChild(el);
-                }))))
+                fetch(e.download_url).then(r =>
+                    r.text().then(t => {
+                        fn(document.createElement(elType), t)
+                    })))))
 }
 
 cdn(
-    "https://api.github.com/repos/JamFactoryInc/krontesite/contents/news",
-    "#news .readable",
-    dce('span')
+    "https://api.github.com/repos/JamFactoryInc/kronitesite/contents/news",
+    "span",
+    (element, text) => {
+        element.innerHTML = text;
+        document.querySelector("#news .readable").appendChild(element);
+    }
+)
+
+cdn(
+    "https://api.github.com/repos/JamFactoryInc/kronitesite/contents/styles",
+    "style",
+    (element, text) => {
+        element.innerHTML = text;
+        document.querySelector("#news .readable").appendChild(element);
+    }
 )
